7a140eb3f34c5d1e49b6aec7c5a853455623568e
feriksan <ferdicool95@gmail.com>
2023-10-17 16:27:52 +0800

    Roles CRUD

---
+++ src/main/java/com/example/testcrud/service/RolesService.java
@@ -0,0 +1,33 @@
+package com.example.testcrud.service;
+
+import com.example.testcrud.entity.Roles;
+import com.example.testcrud.repository.RolesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RolesService {
+    @Autowired
+    RolesRepository rolesRepository;
+    
+    public Roles createRole(Roles roles){return rolesRepository.save(roles);}
+    
+    public List<Roles> findAllRole(){return rolesRepository.findAll();}
+    
+    public List<Roles> findByParentRole(String parentRole){return rolesRepository.findByParentRoleName(parentRole);}
+    
+    public Roles findByRoleName(String role){return rolesRepository.findById(role).orElse(null);}
+    
+    public Roles updateRole(Roles roles){
+        Roles existingRole = rolesRepository.findById(roles.getRoleName()).orElse(null);
+        existingRole.setDescription(roles.getDescription());
+        return rolesRepository.save(existingRole);
+    }
+    
+    public String deleteRole(String roles){
+        rolesRepository.deleteById(roles);
+        return "Role Berhasil Dihapus";
+    }
+}
